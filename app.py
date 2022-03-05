b1ee182c7812831243728688606971d7d3b2bd97
dalred <RcrF63gGG7HpbAT>
2022-03-05 15:25:43 +0300

    init

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
-import os
+import os, json
 
-from flask import Flask
+from flask import Flask, jsonify, request, make_response
 
 app = Flask(__name__)
 
@@ -8,10 +8,19 @@ BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 DATA_DIR = os.path.join(BASE_DIR, "data")
 
 
-@app.route("/perform_query")
+@app.route("/perform_query", methods=['GET'])
 def perform_query():
+    # if request.method == "POST":
+    #     query = request.args.get('query')
+    #     file_name = request.args.get('file_name')
+    #     if os.path.exists(DATA_DIR//file_name):
+
     # получить параметры query и file_name из request.args, при ошибке вернуть ошибку 400
     # проверить, что файла file_name существует в папке DATA_DIR, при ошибке вернуть ошибку 400
     # с помощью функционального программирования (функций filter, map), итераторов/генераторов сконструировать запрос
     # вернуть пользователю сформированный результат
-    return app.response_class('', content_type="text/plain")
+    #return app.response_class('[1, 2, 3]', content_type="text/plain")
+    return json.dumps([1, 2, 3])
+
+if __name__ == '__main__':
+    app.run(host="localhost", port=10001, debug=True)
